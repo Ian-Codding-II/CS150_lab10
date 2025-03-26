2677e20c57407571a16221a52bdb8967b11c5ea9
Ian Codding II <icodding@nic.edu>
2025-03-26 20:49:26 +0000

    third push

--- vector.cpp
+++ vector.cpp
@@ -11,13 +11,14 @@ int main()
     for (int i = 0; i < 10; i++)
     {
         array[i] = i + 1;
-        array[1] = i + 1;
     }
     for (int i = 0; i < 10; i++)
     {
         vec1.push_back(array[i]);
         cout << vec1[i];
+        cout << " ";
     }
+    cout << "\n";
 
     return 0;
 }
